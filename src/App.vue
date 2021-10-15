9b7eaf09126117f81892625cedebd9fa9e643e64
Maggie <1134044314@qq.com>
2021-10-15 15:19:22 +0800

    初始化项目-vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <router-view />
-    <h1>111</h1>
+    <h1>
+      黑马头条
+    </h1>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 <script>
